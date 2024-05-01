153be5081af2edd002ec1a88463416d9537a28d9
snipe <72265661+notsniped@users.noreply.github.com>
2024-05-01 16:16:47 +0530

    Add a module for fetching terminal color codes

---
+++ colors.py
@@ -0,0 +1,11 @@
+"""Library used for stdout colors."""
+
+class Colors:
+    """Contains general stdout colors."""
+
+    cyan = '\033[96m'
+    red = '\033[91m'
+    green = '\033[92m'
+    orange = '\033[33m'
+    yellow = '\033[93m'
+    end = '\033[0m'
